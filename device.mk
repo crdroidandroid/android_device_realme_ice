64f22de7931e7d25451138dcee3accf1e3c7d06b
Sugakesshaa <sugakesshaa@gmail.com>
2023-06-28 20:10:14 +0800

    ice: Set camera id for FaceSense
    Change-Id: I763b080944b598e827458082af5dd3a51031f96c

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
+# FaceSense
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.face.sense_service.camera_id=1
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
