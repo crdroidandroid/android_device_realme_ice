249dbb52f4f0fdf0fd596063caea452995e9ca29
itsxrp <itsxrproms@gmail.com>
2023-11-15 06:49:14 +0000

    ice: Welcome PixelOS
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,7 @@
-#
-# Copyright (C) 2021 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ice.mk
+    $(LOCAL_DIR)/aosp_ice.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ice-user \
-    lineage_ice-userdebug \
-    lineage_ice-eng
+    aosp_ice-user \
+    aosp_ice-userdebug \
+    aosp_ice-eng
