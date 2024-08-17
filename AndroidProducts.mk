9e1b87a544213838bd4a00c1bf4b3cb71827e547
SX <neoroms69@gmail.com>
2024-08-17 14:51:01 +0530

    Revert "ice: Welcome PixelOS"
    This reverts commit 249dbb52f4f0fdf0fd596063caea452995e9ca29.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_ice.mk
+    $(LOCAL_DIR)/lineage_ice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_ice-user \
-    aosp_ice-userdebug \
-    aosp_ice-eng
+    lineage_ice-user \
+    lineage_ice-userdebug \
+    lineage_ice-eng
